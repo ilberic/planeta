9f390e963add173048aeafeb203e024d57eb1670
Vadim <cltcmdc2001@gmail.com>
2020-07-23 18:27:28 +0500

    3

--- js/script.js
+++ js/script.js
@@ -7,13 +7,13 @@ window.addEventListener('DOMContentLoaded', () => {
     let slideIndex = 0;
     function changeSlide () {
         if (slideIndex == 0) {
-            slide.style.background = 'url(../img/slider/1.jpg) center center/cover no-repeat';
+            slide.style.background = 'url(./img/slider/1.jpg) center center/cover no-repeat';
             slideText.textContent = 'Эта элегантная и эмоциональная кухня в стиле модерн—блестящий образец искусства кухонного интерьера. Вся композиция олицетворяет собой целостность: никаких лишних деталей. Столешница из искусственного камня непрерывна по всей длине кухни.';
         } else if (slideIndex == 1) {
-            slide.style.background = 'url(../img/slider/2.jpg) center center/cover no-repeat';
+            slide.style.background = 'url(./img/slider/2.jpg) center center/cover no-repeat';
             slideText.textContent = 'Новая модель кухни изготовленной из пластика высокого давления HPL. Высокая глянцевость и глубина цвета не оставит равнодушным любого покупателя! Так же пластик является самым антивандальным покрытием для фасадов кухни. Его сложно поцарапать или оставить вмятину. Легко чистится как сухим материалом, так и мыльным раствором.';
         } else if (slideIndex == 2) {
-            slide.style.background = 'url(../img/slider/3.png) center center/cover no-repeat';
+            slide.style.background = 'url(./img/slider/3.png) center center/cover no-repeat';
             slideText.textContent = 'Торжество современных форм и смелость цветовых решений. Вариативность комплектации и эффектный дизайн создают сбалансированную модель с выраженной стилевой концепцией.';
         }
     }
@@ -25,7 +25,6 @@ window.addEventListener('DOMContentLoaded', () => {
         } else {
             slideIndex = 0;
         }
-        console.log(slideIndex);
         changeSlide();
     });
     prev.addEventListener('click', () => {
@@ -34,7 +33,6 @@ window.addEventListener('DOMContentLoaded', () => {
         } else {
             slideIndex = 2;
         }
-        console.log(slideIndex);
         changeSlide();
     });
     
